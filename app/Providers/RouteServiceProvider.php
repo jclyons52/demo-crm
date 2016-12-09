e13bebe03469673a5037e20e8057028a08ef4e86
Joseph Lyons <jclyons52@gmail.com>
2016-12-09 22:57:31 +1100

    more vendor assets

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,70 +8,71 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
+    * This namespace is applied to your controller routes.
+    *
+    * In addition, it is set as the URL generator's root namespace.
+    *
+    * @var string
+    */
     protected $namespace = 'App\Http\Controllers';
-
+    
     /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @return void
-     */
+    * Define your route model bindings, pattern filters, etc.
+    *
+    * @return void
+    */
     public function boot()
     {
         //
-
+        
         parent::boot();
     }
-
+    
     /**
-     * Define the routes for the application.
-     *
-     * @return void
-     */
+    * Define the routes for the application.
+    *
+    * @return void
+    */
     public function map()
     {
         $this->mapApiRoutes();
-
+        
         $this->mapWebRoutes();
-
+        
         //
     }
-
+    
     /**
-     * Define the "web" routes for the application.
-     *
-     * These routes all receive session state, CSRF protection, etc.
-     *
-     * @return void
-     */
+    * Define the "web" routes for the application.
+    *
+    * These routes all receive session state, CSRF protection, etc.
+    *
+    * @return void
+    */
     protected function mapWebRoutes()
     {
         Route::group([
-            'middleware' => 'web',
-            'namespace' => $this->namespace,
+        'middleware' => 'web',
+        'namespace' => $this->namespace,
         ], function ($router) {
             require base_path('routes/web.php');
         });
     }
-
+    
     /**
-     * Define the "api" routes for the application.
-     *
-     * These routes are typically stateless.
-     *
-     * @return void
-     */
+    * Define the "api" routes for the application.
+    *
+    * These routes are typically stateless.
+    *
+    * @return void
+    */
     protected function mapApiRoutes()
     {
         Route::group([
-            'middleware' => 'api',
-            'namespace' => $this->namespace,
-            'prefix' => 'api',
+        'middleware' => 'api',
+        'namespace' => $this->namespace."\\API",
+        'prefix' => 'api',
+        'as' => 'api.',
         ], function ($router) {
             require base_path('routes/api.php');
         });
